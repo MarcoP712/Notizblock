c6bbedf7c8e7bdcda0637f4a272439f7aa2f8cc7
Marco Pruckner <marco.pruckner2002@gmail.com>
2026-03-31 21:59:35 +0200

    enter the script and css

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-
-
- let notes = ['banana', 'rasen mähen'];
+let notes = ['banana', 'rasen mähen', 'orange'];
 
 function renderNotes() {
     let contentRef = document.getElementById('content');
-    contentRef.innerHTML = notes;
+    contentRef.innerHTML = '';
+
+    for (let i = 0; i < notes.length; i++) {
+        contentRef.innerHTML += `<div class="note">+ ${notes[i]}</div>`;
+    }
 }
 
-// notizen hinzufügen
-// notizen löschen
-// notizen archivieren
+function addNote() {
+    let noteInputRef = document.getElementById('noteInput');
+    let newNote = noteInputRef.value.trim();
+
+    if (newNote !== '') {
+        notes.push(newNote);
+        noteInputRef.value = '';
+        renderNotes();
+    }
+}
