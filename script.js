f2c5371df80ba553ba466fac656370b6b1ce7323
Marco Pruckner <marco.pruckner2002@gmail.com>
2026-03-31 21:47:00 +0200

    add new function

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+ let notes = ['banana', 'rasen mähen'];
+
+function renderNotes() {
+    let contentRef = document.getElementById('content');
+    contentRef.innerHTML = notes;
+}
+
+// notizen hinzufügen
+// notizen löschen
+// notizen archivieren
