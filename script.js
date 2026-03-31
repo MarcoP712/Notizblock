cdd74120b9d4c6eacb50bfe63acc1b992a3d5a6e
Marco Pruckner <marco.pruckner2002@gmail.com>
2026-03-31 22:13:49 +0200

    delete buton and css change

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-let notes = ['banana', 'rasen mähen', 'orange'];
+let notes = [];
 
 function renderNotes() {
     let contentRef = document.getElementById('content');
     contentRef.innerHTML = '';
 
     for (let i = 0; i < notes.length; i++) {
-        contentRef.innerHTML += `<div class="note">+ ${notes[i]}</div>`;
+        contentRef.innerHTML += `
+            <div class="note">
+                + ${notes[i]}
+                <button class="delete-btn" onclick="deleteNote(${i})">X</button>
+            </div>
+        `;
     }
 }
 
 function addNote() {
-    let noteInputRef = document.getElementById('noteInput');
-    let newNote = noteInputRef.value.trim();
+    let inputRef = document.getElementById('noteInput');
+    let newNote = inputRef.value.trim();
 
     if (newNote !== '') {
         notes.push(newNote);
-        noteInputRef.value = '';
+        inputRef.value = '';
         renderNotes();
     }
+}
+
+function deleteNote(index) {
+    notes.splice(index, 1);
+    renderNotes();
 }
